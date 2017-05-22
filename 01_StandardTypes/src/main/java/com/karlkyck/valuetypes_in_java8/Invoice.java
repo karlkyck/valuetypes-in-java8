259bdae180abd533ea6b0e9e31e99a3f4dbfb84b
Karl Kyck <karlkyck@gmail.com>
2017-05-22 20:25:35 +0100

    Refactored code style for the blog.

--- 01_StandardTypes/src/main/java/com/karlkyck/valuetypes_in_java8/Invoice.java
+++ 01_StandardTypes/src/main/java/com/karlkyck/valuetypes_in_java8/Invoice.java
@@ -8,17 +8,38 @@ public class Invoice {
 
     public static void main(String[] args) {
 
-        InvoiceItem hardWork = new InvoiceItem("Hard work!", 40.1, 5.0125);
-        InvoiceItem evenHarderWork = new InvoiceItem("Even Harder work!", 60.2, 10.535);
-        InvoiceItem superHardWork = new InvoiceItem("Super Harder work!", 100.3, 23.069);
+        InvoiceItem hardWork = new InvoiceItem(
+                "Hard work!",
+                40.1,
+                5.0125
+        );
+        InvoiceItem evenHarderWork = new InvoiceItem(
+                "Even Harder work!",
+                60.2,
+                10.535
+        );
+        InvoiceItem superHardWork = new InvoiceItem(
+                "Super Harder work!",
+                100.3,
+                23.069
+        );
 
-        List<InvoiceItem> invoiceItems = Collections.unmodifiableList(Arrays.asList(
-                hardWork,
-                evenHarderWork,
-                superHardWork));
+        List<InvoiceItem> invoiceItems = Collections.unmodifiableList(
+                Arrays.asList(
+                        hardWork,
+                        evenHarderWork,
+                        superHardWork
+                ));
 
-        double totalAmount = invoiceItems.stream().mapToDouble(value -> value.vatAmount).sum();
-        double totalVatAmount = invoiceItems.stream().mapToDouble(value -> value.amount).sum();
+        double totalAmount = invoiceItems
+                .stream()
+                .mapToDouble(value -> value.vatAmount)
+                .sum();
+
+        double totalVatAmount = invoiceItems
+                .stream()
+                .mapToDouble(value -> value.amount)
+                .sum();
 
         System.out.println(String.format("Total invoice amount is %s", totalAmount));
         System.out.println(String.format("Total invoice vat amount is %s", totalVatAmount));
--- 04_Validation/src/main/java/com/karlkyck/valuetypes_in_java8/Invoice.java
+++ 04_Validation/src/main/java/com/karlkyck/valuetypes_in_java8/Invoice.java
@@ -7,17 +7,44 @@ import java.util.List;
 public class Invoice {
 
     public static void main(String[] args) {
-        InvoiceItem hardWork = new InvoiceItem("Hard work!", Amount.of(40.1), VatAmount.of(5.0125));
-        InvoiceItem evenHarderWork = new InvoiceItem("Even Harder work!", Amount.of(60.2), VatAmount.of(10.535));
-        InvoiceItem superHardWork = new InvoiceItem("Super Harder work!", Amount.of(100.3), VatAmount.of(23.069));
+        InvoiceItem hardWork = new InvoiceItem(
+                "Hard work!",
+                Amount.of(40.1),
+                VatAmount.of(5.0125)
+        );
+        InvoiceItem evenHarderWork = new InvoiceItem(
+                "Even Harder work!",
+                Amount.of(60.2),
+                VatAmount.of(10.535)
+        );
+        InvoiceItem superHardWork = new InvoiceItem(
+                "Super Harder work!",
+                Amount.of(100.3),
+                VatAmount.of(23.069)
+        );
 
-        List<InvoiceItem> invoiceItems = Collections.unmodifiableList(Arrays.asList(
-                hardWork,
-                evenHarderWork,
-                superHardWork));
+        List<InvoiceItem> invoiceItems = Collections.unmodifiableList(
+                Arrays.asList(
+                        hardWork,
+                        evenHarderWork,
+                        superHardWork
+                ));
 
-        Amount totalAmount = invoiceItems.stream().reduce(Amount.ZERO, (amount, invoiceItem) -> amount.add(invoiceItem.amount), Amount::add);
-        VatAmount totalVatAmount = invoiceItems.stream().reduce(VatAmount.ZERO, (amount, invoiceItem) -> amount.add(invoiceItem.vatAmount), VatAmount::add);
+        Amount totalAmount = invoiceItems
+                .stream()
+                .reduce(
+                        Amount.ZERO,
+                        (amount, invoiceItem) -> amount.add(invoiceItem.amount),
+                        Amount::add
+                );
+
+        VatAmount totalVatAmount = invoiceItems
+                .stream()
+                .reduce(
+                        VatAmount.ZERO,
+                        (amount, invoiceItem) -> amount.add(invoiceItem.vatAmount),
+                        VatAmount::add
+                );
 
         System.out.println(String.format("Total invoice amount is %s", totalAmount));
         System.out.println(String.format("Total invoice vat amount is %s", totalVatAmount));
